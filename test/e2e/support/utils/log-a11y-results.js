80173d6e7236bc64c56ec9aa5e83f4923f63d468
Stephen Mathieson <me@stephenmathieson.com>
2018-12-09 13:08:11 -0600

    test: add several more a11y checks

--- test/e2e/support/utils/log-a11y-results.js
+++ test/e2e/support/utils/log-a11y-results.js
@@ -7,8 +7,8 @@ export const logA11yResults = ( a11yResults ) => {
 	}
 
 	for ( const violation of violations ) {
-		const selectors = violation.nodes.map( ( node ) => `${ node.target }` ).join( ', ' );
-		process.stderr.write( `[${ chalk.bold.red( 'a11y' ) }] ${ violation.id }: ${ violation.description }  (elements ${ selectors })` );
+		const selectors = violation.nodes.map( ( node ) => `"${ node.target }"` ).join( ', ' );
+		process.stderr.write( `[${ chalk.bold.red( 'a11y' ) }] ${ violation.id }: ${ violation.description } (elements ${ selectors })` );
 	}
 	process.stderr.write( '\n' );
 };
